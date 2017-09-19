9f9517b999b44297082ca33ce53273cde116c789
Sahib Arora <sahibarora1997@gmail.com>
2017-09-19 15:51:48 -0400

    Add files via upload

---
+++ Streamable.h
@@ -0,0 +1,18 @@
+// this is an abstract class which will be further used in one of the class of the app...
+
+#ifndef SICT_STREAMABLE_H__
+#define SICT_STREAMABLE_H__
+#include<iostream>
+#include<fstream>
+namespace sict
+{
+	class Streamable
+	{
+	public:
+		virtual std::fstream& store(std::fstream& file, bool addNewLine = true)const = 0;
+		virtual std::fstream& load(std::fstream& file) = 0;
+		virtual std::ostream& write(std::ostream& os, bool linear)const = 0;
+		virtual std::istream& read(std::istream& is) = 0;
+	};
+}
+#endif
